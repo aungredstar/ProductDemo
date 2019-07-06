4ec8c9001036dd08d1d91633b92a3f2a13df08fc
aungredstar <aung.redstar@gmail.com>
2019-07-06 22:38:13 +0800

    Add files via upload

---
+++ ProductManagement/ProductManagement/Index.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Language="C#" Async="true" AutoEventWireup="true" CodeBehind="Index.aspx.cs" EnableEventValidation="false" Inherits="ProductManagement.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css" />
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css" />
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css" />
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css" />
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css" />
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="css/util.css" />
+        <link rel="stylesheet" type="text/css" href="css/main.css" />
+        <!--===============================================================================================-->
+
+
+        <div class="limiter">
+            <div class="table100 ver1" style="padding-top: 0px !important; margin-top:20px; margin-bottom: 20px;">
+                <table border="0" cell-spacing="2">
+                    <tr>
+                        <th colspan="2" style="text-align:center;"> Project Management</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label class="control-label">Name</label>
+                        </td>
+                        <td>
+                            <asp:TextBox ID="txtName" runat="server" CssClass="form-control"></asp:TextBox>
+                            <asp:HiddenField ID="hidId" runat="server" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label class="control-label">Quantity</label></td>
+                        <td>
+                            <asp:TextBox ID="txtQuanity" runat="server" CssClass="form-control"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label class="control-label">Sale Amount</label>
+                        </td>
+                        <td>
+                            <asp:TextBox ID="txtSaleAmount" runat="server" CssClass="form-control"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td style="text-wrap: none;">
+                            <asp:Button ID="btnSave" Text="Save" runat="server" CssClass="btn btn-light" OnClick="btnSave_Click" Visible="false" />
+                            <asp:Button ID="btnAddNew" Text="Add New" runat="server" CssClass="btn btn-light" OnClick="btnAddNew_Click" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"></td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="table100 ver1" style="padding-top: 0px !important; margin-bottom: 20px;">
+                <asp:GridView ID="gv" runat="server" AutoGenerateColumns="false" BorderWidth="0" OnRowCommand="gv_RowCommand" RowStyle-CssClass="row100 body">
+                    <Columns>
+                        <asp:TemplateField ItemStyle-Width="20" ItemStyle-HorizontalAlign="Center" ItemStyle-Wrap="false">
+                            <ItemTemplate>
+                                <asp:ImageButton ID="ibtnEdit" runat="server" CommandName="CHANGE" Style="display: normal !important; margin-left: 4px;" CommandArgument='<%# Eval("Id") %>' OnClientClick="" ImageUrl="Images/edit.gif" />
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField ItemStyle-Width="20" ItemStyle-HorizontalAlign="Center" ItemStyle-Wrap="false">
+                            <ItemTemplate>
+                                <asp:ImageButton ID="ibtnDel" runat="server" CommandName="DEL" Style="display: normal !important;" CommandArgument='<%# Eval("Id") %>' Width="16" Height="16" OnClientClick="" ImageUrl="Images/f_delete.png" />
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Name">
+                            <ItemTemplate>
+                                <asp:Label ID="lblName" runat="server" Text='<%# Eval("Name") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Quantity">
+                            <ItemTemplate>
+                                <asp:Label ID="lblQuantity" runat="server" Text='<%# Eval("Quantity") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Sale Amount">
+                            <ItemTemplate>
+                                <asp:Label ID="lblSaleAmount" runat="server" Text='<%# Eval("SaleAmount") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ ProductManagement/ProductManagement/Index.aspx.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+using Newtonsoft.Json;
+using Product.Shared.Items;
+using Product.Shared.Requests;
+using Product.Shared.Responses;
+
+namespace ProductManagement
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        public string BaseApiUrl = "http://localhost/ProductApi/api";
+
+        private string EventId
+        {
+            get
+            {
+                if (ViewState["PROD_EVENT_ID"] == null)
+                    return "";
+
+                return ViewState["PROD_EVENT_ID"].ToString();
+            }
+            set { ViewState["PROD_EVENT_ID"] = value; }
+        }
+        #region Page Events
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                EventId = Guid.NewGuid().ToString();
+                BindData();
+            }
+        }
+
+        #endregion
+
+        #region Control Events
+
+        protected async void gv_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName.Trim().Equals("CHANGE"))
+            {
+                var ctrl = e.CommandSource as ImageButton;
+
+                if (ctrl != null)
+                {
+                    var row = ctrl.Parent.NamingContainer as GridViewRow;
+                    var lblName = (Label)row.FindControl("lblName");
+                    var lblQuantity = (Label)row.FindControl("lblQuantity");
+                    var lblSaleAmount = (Label)row.FindControl("lblSaleAmount");
+
+                    hidId.Value = e.CommandArgument.ToString().Trim();
+                    txtName.Text = lblName.Text.Trim();
+                    txtQuanity.Text = lblQuantity.Text.Trim();
+                    txtSaleAmount.Text = lblSaleAmount.Text.Trim();
+
+                    btnSave.Visible = true;
+                }
+            }
+            else if (e.CommandName.Trim().Equals("DEL"))
+            {
+                await Delete(int.Parse(e.CommandArgument.ToString()));
+                BindData();
+            }
+        }
+
+        protected async void btnSave_Click(object sender, EventArgs e)
+        {
+            var item = GetItem();
+            item.Id = int.Parse(hidId.Value);
+
+            var result = await Save(item);
+
+            BindData();
+            ClearControl();
+        }
+
+        protected async void btnAddNew_Click(object sender, EventArgs e)
+        {
+            var item = GetItem();
+
+            var result = await Save(item);
+
+            BindData();
+            ClearControl();
+        }
+
+        #endregion
+
+        #region Functions
+
+        private async Task<ProductResponse> Save(ProductItem item)
+        {
+            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            var request = new ProductRequest
+            {
+                Id = EventId,
+                Timestamp = unixTimestamp.ToString(),
+                Product = item
+            };
+
+            string json = await GetPost("Product", "SaveProduct", request);
+
+            dynamic obj = JsonConvert.DeserializeObject(json);
+
+            ProductResponse response = obj == null ? null : obj.ToObject<ProductResponse>();
+
+            return response;
+        }
+
+        private async Task<ProductResponse> Delete(int id)
+        {
+            var item = new ProductItem
+            {
+                Id = id
+            };
+            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            var request = new ProductRequest
+            {
+                Id = EventId,
+                Timestamp = unixTimestamp.ToString(),
+                Product = item
+            };
+
+            string json = await GetPost("Product", "Delete", request);
+
+            dynamic obj = JsonConvert.DeserializeObject(json);
+
+            ProductResponse response = obj == null ? null : obj.ToObject<ProductResponse>();
+
+            return response;
+        }
+
+        private ProductItem GetItem()
+        {
+            var item = new ProductItem();
+            
+            item.Name = txtName.Text.Trim();
+
+            if (!string.IsNullOrEmpty(txtQuanity.Text.Trim()))
+                item.Quantity = int.Parse(txtQuanity.Text.Trim());
+
+            if (!string.IsNullOrEmpty(txtSaleAmount.Text.Trim()))
+                item.SaleAmount = decimal.Parse(txtSaleAmount.Text.Trim());
+
+            return item;
+        }
+
+        private void ClearControl()
+        {
+            hidId.Value = "";
+            txtName.Text = "";
+            txtQuanity.Text = "";
+            txtSaleAmount.Text = "";
+            btnSave.Visible = false;
+        }
+
+        private async void BindData()
+        {
+            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            var request = new ProductRequest
+            {
+                Id = EventId,
+                Timestamp = unixTimestamp.ToString()
+            };
+
+            string json = await GetContent("Product", "GetProducts", request);
+
+            dynamic obj = JsonConvert.DeserializeObject(json);
+
+            ProductResponse response = obj == null ? null : obj.ToObject<ProductResponse>();
+
+            if (response != null)
+            {
+                gv.DataSource = response.Products;
+                gv.DataBind();
+            }
+        }
+
+        public async Task<string> GetPost(string serviceName, string methodName, object item)
+        {
+            try
+            {
+                var _client = new HttpClient();
+                var uri = new Uri(string.Format(GetServiceUrl(serviceName, methodName), string.Empty));
+                var json = JsonConvert.SerializeObject(item);
+                var param = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await _client.PostAsync(uri, param);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return content;
+                }
+
+                return "";
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public async Task<string> GetContent(string serviceName, string methodName, object item)
+        {
+            try
+            {
+                var _client = new HttpClient();
+                var uri = new Uri(string.Format(GetServiceUrl(serviceName, methodName), string.Empty));
+                var json = JsonConvert.SerializeObject(item);
+                var param = new StringContent(json, Encoding.UTF8, "application/json");
+                
+                var response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return content;
+                }
+
+                return "";
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public string GetServiceUrl(string serviceName, string methodName = null)
+        {
+            var url = string.Format("{0}/{1}", BaseApiUrl, serviceName);
+
+            if (!string.IsNullOrWhiteSpace(methodName))
+                url = string.Format("{0}/{1}", url, methodName);
+
+            return url;
+        }
+
+        #endregion
+    }
+}
